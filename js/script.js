5effbe3646b1dab180df3025c621648c2e64008e
SterrenHeer <sterrenheer@gmail.com>
2024-07-22 12:58:51 +0300

    add slider to weather section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $.get('footer.html',function(response){ 
     $('.footer').html(response); 
 });
+
+if (document.querySelector('.weather_slider') != null)
+slider({
+    containerSelector: '.weather_slider',
+    slideSelector: '.weather_slide',
+    nextSlideSelector: '.weather_next',
+    prevSlideSelector: '.weather_prev',
+    wrapperSelector: '.weather_slider',
+    fieldSelector: '.weather_field',
+    indicatorsClass: 'weather_indicators',
+    swipe: true,
+});
