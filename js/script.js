5a96810d875e872b8d41c5a8182f550c47474ca6
SterrenHeer <sterrenheer@gmail.com>
2024-07-22 16:51:30 +0300

    refine header section with burger menu and background on scroll

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
+$.get('header.html',function(response){ 
+    $('.header').html(response); 
+    $('.burger_button, .burger_close, .burger a').click(() => {
+        $('.burger').toggleClass('show');
+    });
+    if (document.querySelector('.burger') != null) {
+        modal('.burger_button', 'data-close', '.burger');
+    }
+});
 $.get('footer.html',function(response){ 
     $('.footer').html(response); 
 });
+
+window.onscroll = function() {
+    let header = document.querySelector(".header_main .container");
+    window.scrollY > 200 ? header.classList.add('blur') : header.classList.remove('blur');
+}
 
 if (document.querySelector('.weather_slider') != null)
 slider({
